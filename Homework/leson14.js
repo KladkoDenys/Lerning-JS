3d044841e363971f5287d6082a62e2052ca9b8dc
Kladko Denis <kladko.denys@gmail.com>
2023-01-24 14:25:27 +0200

    Add leson 14

---
+++ Homework/leson14.js
@@ -0,0 +1,26 @@
+const myCity = {
+	city : 'NY'
+}
+function show(){
+	console.log(myCity);
+}
+show();
+myCity.country = 'US';
+show();
+myCity.popular = true;
+show();
+delete myCity.country;
+show();
+delete myCity.popular;
+show();
+const namePopular = 'popular';
+myCity[namePopular] = true;
+show();
+nameCountry = 'country';
+myCity[nameCountry] = 'USA';
+show();
+delete myCity.country;
+show();
+const nameColor = 'color';
+myCity[nameColor] ='red';
+show();
