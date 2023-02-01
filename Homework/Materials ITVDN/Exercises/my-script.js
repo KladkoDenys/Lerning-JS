3276451a1abffd462b2eeffb1833e3dbbe3681ae
Kladko Denis <kladko.denys@gmail.com>
2023-02-01 16:30:11 +0200

    class

--- Homework/Materials ITVDN/Exercises/my-script.js
+++ Homework/Materials ITVDN/Exercises/my-script.js
@@ -210,4 +210,19 @@
 		// }
 
 		// -----------------------------------------------------
-		
+		// const myObject = {
+		// 	x: 10,
+		// 	y: true,
+		// 	z: 'abc'
+		// }
+
+		// for(const key in myObject) {
+		// 	console.log(key, myObject[key])
+		// }
+// -----------------------------------------------------------------
+
+// const myString = 'Hey'
+// for (const letter of myString) {
+// 	console.log(letter)
+// }
+// ---------------------------------------------------
---
+++ Homework/module2.mjs
@@ -0,0 +1,7 @@
+import {
+	sum,
+	mult as multNumber
+} from './module1.mjs'
+
+console.log(sum(2,5))
+console.log(multNumber(2,5))
