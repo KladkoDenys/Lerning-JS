303b63c7b83ff3a1e15fca27d3e5f2a219145328
Kladko Denys <kladko.denys@gmail.com>
2023-02-22 16:23:46 +0200

    rangom gen. array

--- Homework/index.js
+++ Homework/index.js
@@ -18,7 +18,25 @@
 
 // showCity();
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(err => console.log(err.message))
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.log(err.message))
+
+
+
+let randomArray = (arrayQuantity, min, max) => {
+  
+  let generatedArray = []
+  
+  for (let i = 0; i < arrayQuantity; i++) {
+    generatedArray.push(Math.floor(Math.random() * (max - min) + min))
+  }
+   
+  return generatedArray;
+} 
+
+let arrayLeson1 = randomArray(8, 1, 10);
+
+
+console.log(arrayLeson1);
